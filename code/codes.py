ad43514b079ac954c68080e266439a5ec2d28ed7
Yunshu Zhang <jyszhang2020@gmail.com>
2021-05-08 23:50:43 -0500

    Auth Updated-YJZ

--- code/codes.py
+++ code/codes.py
@@ -1,9 +1,9 @@
 # # Go to http://apps.twitter.com and create an app.
 # # The consumer key and secret will be generated for you after
-consumer_key='YRiabHb1FHrpmkmjN2NmJ7mxs'
-consumer_secret='D1BGaA3EJJMVzmsxonlIGGEJX9La9w1fxmCf7mzFEczZUKrMpR'
+consumer_key='daaTXamW4zoaJ2VsQVwNyrzv8'
+consumer_secret='7gw90naSNKmvrKPnTy5aRhwCWurt8nMdPVRfzDIqpk3mrfCgeX'
 
 # # After the step above, you will be redirected to your app's page.
 # # Create an access token under the the "Your access token" section
-access_token='601447454-HyKn8Ekz6fgcHJkRwpJAUtxLQAIE4ffQiAKphJEH'
-access_token_secret='ZsGNRNvYi6bmfaK5wuO8rT7AGb1Wzx6YT5HNtGrNWOULZ'
+access_token='1641426878-2qlQzZz26UyJsMxxC6uQNaiTLDz5GGPQ0ZGZ4wY'
+access_token_secret='1jiPs7H4GvvELHJhS0a6HPhmf4D0pYdiO4HR2wAAoLVRm'
